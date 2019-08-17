ad1d8f717b6cf4a49db9ee5148f5edd97094bdf6
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-08-17 10:24:39 -0700

    creating file paths, and buildling NavBar

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import NavBar from "./Components/NavBar"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <Route path="" />
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
