a9a4e96f006d2fd4c8be786cdade70bbd93e813a
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-08-18 17:56:51 -0700

    delete bootstrap and built nav from scratch
    styled, and implimented an image (will change to be my logo eventually)

--- my-portfolio/src/Components/NavBar.js
+++ my-portfolio/src/Components/NavBar.js
@@ -1,19 +1,40 @@
 import React from "react"
-import Navbar from "react-bootstrap/Navbar"
-import Nav from "react-bootstrap/Nav"
+import styled from "styled-components"
 
-const NavBar = () => {
-  return (
-    <>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Shelby Diamond</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">About</Nav.Link>
-          <Nav.Link href="#features">Projects</Nav.Link>
-          <Nav.Link href="#pricing">Contact</Nav.Link>
-        </Nav>
-      </Navbar>
-    </>
-  )
+const Nav = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  padding: 35px;
+  background: purple;
+  justify-content: space-evenly;
+`
+const Anchor = styled.a`
+  text-decoration: none;
+  color: white;
+  font-size: 1.2rem;
+  &:nth-child(1) {
+    text-align: left;
+    margin-right: 75px;
+  }
+  &:nth-child(2) {
+    text-align: right;
+  }
+  &:nth-child(3) {
+    text-align: right;
+  }
+`
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <Nav>
+        <img src="../images/capture" />
+        <Anchor href="#">Shelby Diamond</Anchor>
+        <Anchor href="#">About</Anchor>
+        <Anchor href="#">Projects</Anchor>
+      </Nav>
+    )
+  }
 }
 export default NavBar
