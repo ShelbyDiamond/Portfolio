da118ebf4a2033a0f9ab25e4305040ff4d73ebac
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-08-31 21:26:45 -0700

    updated styles on contact page

--- my-portfolio/src/Components/Contact.js
+++ my-portfolio/src/Components/Contact.js
@@ -2,7 +2,13 @@ import React from "react"
 import Github from "../images/Github.PNG"
 import Twitter from "../images/twitter.PNG"
 import LinkedIn from "../images/LinkedIn.PNG"
+import styled from "styled-components"
 
+const Icon = styled.img`
+  width: 45px;
+  height: 45px;
+  padding: 15px;
+`
 const Contact = () => {
   return (
     <div>
@@ -10,9 +16,9 @@ const Contact = () => {
       <p>Shelby Diamond</p>
       <p>shelbydiamond518@gmail.com</p>
 
-      <img src={Github} alt="Github Logo" />
-      <img src={Twitter} alt="Twitter Logo" />
-      <img src={LinkedIn} alt="LinkedIn Logo" />
+      <Icon src={Github} alt="Github Logo" />
+      <Icon src={Twitter} alt="Twitter Logo" />
+      <Icon src={LinkedIn} alt="LinkedIn Logo" />
     </div>
   )
 }
