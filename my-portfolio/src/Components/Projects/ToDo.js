92b2f6aa1d045bc6df4006615f5a6d33fa07c24d
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-11-11 09:32:57 -0700

    updated project cards to have tech used

--- my-portfolio/src/Components/Projects/ToDo.js
+++ my-portfolio/src/Components/Projects/ToDo.js
@@ -1,35 +1,40 @@
 import React from "react"
-import image from "../../images/ToDoImage.png"
+import image from "../../images/MiracleMessages.PNG"
 import {
   Buttons,
   Description,
   Heading1,
   BigContainer,
   Image1,
-  ButtonDiv
+  ButtonDiv,
+  TechDiv,
+  TechParagraph
 } from "../Styles/styles"
 
 const ToDo = () => {
   return (
     <BigContainer>
-      <Heading1>To-Do List</Heading1>
+      <Heading1>Miracle Messages</Heading1>
+      <TechDiv>
+        <TechParagraph>Front End || React | Redux</TechParagraph>
+      </TechDiv>
+      <TechDiv>
+        <TechParagraph>Back End || Node</TechParagraph>
+      </TechDiv>
       <div>
-        <Image1 src={image} alt="screen shot of my finished project" />
+        <Image3 src={image} alt="screen shot of my finished project" />
       </div>
       <Description>
-        In this project I used react to create a To-Do list. This can help keep
-        track of the chores you need to do, and the ones you have already
-        completed. Once you have completed a chore you can check off that it is
-        finished. When enough of them (or even one) is completed you will be
-        able to use the Clear button to delete all the chores that are
-        completed.
+        Our team built a functioning map for the Miracle Messages organization
+        to use. It shows the locations of the chapters, number of volunteers per
+        chapter, and number/locations of the reunions.
       </Description>
       <ButtonDiv>
-        <Buttons href="https://react-todo.shelbydiamond.now.sh" target="_blank">
+        <Buttons href="#" target="_blank">
           Live Site
         </Buttons>
         <Buttons
-          href="https://github.com/ShelbyDiamond/React-Todo"
+          href="https://github.com/orgs/Lambda-School-Labs/teams/labs-18-miracle-messages/repositories"
           target="_blank"
         >
           GitHub Repo
